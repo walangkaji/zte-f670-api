b5c3d1860984ce0fa34d7177d2e332db667ba06c
Walangkaji <mewalangkaji@gmail.com>
2020-05-04 09:18:12 +0700

    bismillah

--- README.md
+++ README.md
@@ -1,1 +1,116 @@
-zte-f670-api
+# ZTE F670 UNOFFICIAL API
+
+Library ini merupakan **Emboh**. TITIK
+
+# Support me
+- ![Paypal](https://raw.githubusercontent.com/walangkaji/emboh/master/img/paypal.png) Paypal: [Se-Ikhlasnya Saja](https://www.paypal.me/walangkaji)
+
+
+## Install
+#### Composer
+```sh
+$ composer require walangkaji/zte-f670-api
+```
+#### Clone
+```sh
+$ git clone https://github.com/walangkaji/zte-f670-api.git
+$ cd zte-f670-api/
+$ composer install
+```
+
+## Cara Pakai
+```php
+require 'vendor/autoload.php';
+
+use walangkaji\ZteF670\ZteF670;
+
+$ipModem  = '192.168.1.1';
+$username = 'admin';
+$password = 'password';
+$debug    = false;
+
+$zteF670  = new ZteF670($ipModem, $username, $password, $debug);
+$login    = $zteF670->login();
+
+var_dump($login);
+// Terusno dewe
+
+```
+Jika pengen menggunakan proxy:
+```php
+$ipModem  = '192.168.1.1';
+$username = 'admin';
+$password = 'password';
+$debug    = false;
+$proxy    = 'xxx.xxx.xxx.xxx:xxxx'
+
+$zteF670  = new ZteF670($ipModem, $username, $password, $debug, $proxy);
+$login    = $zteF670->login();
+
+var_dump($login);
+// Terusno dewe
+
+```
+
+### Contoh Untuk Reboot / Restart modem
+
+```php
+$zteF670  = new ZteF670($ipModem, $username, $password);
+$login    = $zteF670->login();
+
+if (!$login) {
+    echo 'Login gagal' . PHP_EOL;
+    exit();
+}
+
+$reboot = $zteF670->reboot();
+if ($reboot) {
+    echo 'Berhasil reboot modem.' . PHP_EOL;
+}
+```
+### Contoh Untuk Get Device Information
+
+```php
+$zteF670  = new ZteF670($ipModem, $username, $password);
+$login    = $zteF670->login();
+$info     = $zteF670->status->deviceInformation();
+
+var_dump($info);
+```
+### Contoh Untuk Pon Information
+
+```php
+$zteF670  = new ZteF670($ipModem, $username, $password);
+$login    = $zteF670->login();
+$info     = $zteF670->status->NetworkInterface->ponInformation();
+
+var_dump($info);
+```
+
+## Available Methods
+```php
+$zteF670->login();
+$zteF670->reboot();
+$zteF670->status->deviceInformation();
+$zteF670->status->voIpStatus();
+$zteF670->status->NetworkInterface->wanConnection();
+$zteF670->status->NetworkInterface->wanConnection3Gor4G();
+$zteF670->status->NetworkInterface->tunnelConnection4in6();
+$zteF670->status->NetworkInterface->tunnelConnection6in4();
+$zteF670->status->NetworkInterface->ponInformation();
+$zteF670->status->NetworkInterface->mobileNetwork();
+$zteF670->status->UserInterface->wlan();
+$zteF670->status->UserInterface->ethernet();
+$zteF670->status->UserInterface->usb();
+```
+
+## TODO
+Masih buuuwanyak fitur yang bisa di masukkan. Nanti sambil nangis dikerjakan.
+Pada dasarnya ini dibuat karena kebutuhan. Nek ra butuh yo ra dibuat.
+Apabila kalian pengen nambahin ya monggo dengan senang hati akan diterima.
+
+
+Cukup sekian dan Matursuwun.
+
+Jangan lupa kalo mau support seikhlasnya bisa lewat sini:
+- ![Paypal](https://raw.githubusercontent.com/walangkaji/emboh/master/img/paypal.png) Paypal: [Se-Ikhlasnya Saja](https://www.paypal.me/walangkaji)
---
+++ src/ZteF670.php
@@ -0,0 +1,210 @@
+<?php
+/**
+ * @author walangkaji (https://github.com/walangkaji)
+ */
+
+namespace walangkaji\ZteF670;
+
+require_once 'simple_html_dom.php';
+
+use walangkaji\ZteF670\GlobalFunction as Func;
+
+/**
+ * ZteF670
+ *
+ * CATATAN FENTING DEK:
+ * - Library ini dibuat tidak untuk kejahatan ataupun kegiatan yang merugikan orang lain,
+ *   apalagi untuk usil ke teman atau sanak saudara, itu dosa dek. Mending langsung gelut aja.
+ *
+ * @author walangkaji (https://github.com/walangkaji)
+ */
+class ZteF670
+{
+    public function __construct($ipModem, $username, $password, $debug = false, $proxy = null)
+    {
+        $this->username = $username;
+        $this->password = $password;
+        $this->proxy    = $proxy;
+
+        if ($debug) {
+            $this->debug = true;
+        }
+
+        $this->modemUrl = "http://$ipModem";
+
+        $this->status   = new Request\Status\Status($this);
+    }
+
+    /**
+     * Fungsi untuk login
+     *
+     * @return bool
+     */
+    public function login()
+    {
+        $cekLogin = $this->cekLogin();
+
+        if ($cekLogin) {
+            $this->debug(__FUNCTION__, 'Login session masih aktif.');
+
+            return true;
+        }
+
+        $get  = $this->request($this->modemUrl);
+        $rand = rand(10000000, 99999999);
+
+        $postdata = [
+            'frashnum'       => '',
+            'action'         => 'login',
+            'Frm_Logintoken' => Func::getLoginToken($get),
+            'Username'       => $this->username,
+            'Password'       => md5($this->password . $rand),
+            'UserRandomNum'  => $rand,
+        ];
+
+        $options = [
+            'method'   => 'post',
+            'postdata' => $postdata,
+        ];
+
+        $this->request($this->modemUrl, $options);
+        $cekLogin = $this->cekLogin();
+
+        if ($cekLogin) {
+            $this->debug(__FUNCTION__, 'Berhasil login dengan user ' . $this->username);
+        } else {
+            $this->debug(__FUNCTION__, 'Gagal login dengan user ' . $this->username);
+        }
+
+        return $cekLogin;
+    }
+
+    /**
+     * Fungsi untuk reboot modem
+     *
+     * @return bool
+     */
+    public function reboot()
+    {
+        $get = $this->request($this->modemUrl . Constants::REBOOT);
+
+        $postdata = [
+            'IF_ACTION'      => 'devrestart',
+            'IF_ERRORSTR'    => 'SUCC',
+            'IF_ERRORPARAM'  => 'SUCC',
+            'IF_ERRORTYPE'   => -1,
+            'flag'           => 1,
+            '_SESSION_TOKEN' => Func::find($get, 'session_token = "', '";'),
+        ];
+
+        $options = [
+            'method'   => 'post',
+            'postdata' => $postdata,
+        ];
+
+        $request = $this->request($this->modemUrl . Constants::REBOOT, $options);
+        $cek     = Func::find($request, "flag','", "'");
+
+        if ($cek == 1) {
+            $this->debug(__FUNCTION__, 'Berhasil Reboot modem.');
+
+            return true;
+        }
+
+        $this->debug(__FUNCTION__, 'Gagal Reboot modem.');
+
+        return false;
+    }
+
+    /**
+     * Fungsi untuk cek login
+     *
+     * @return bool
+     */
+    private function cekLogin()
+    {
+        $url      = $this->modemUrl . Constants::TEMPLATE;
+        $response = $this->request($url);
+
+        if (strpos($response, 'logout_redirect') !== false) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Untuk debug proses
+     *
+     * @param mixed $function
+     * @param mixed $text
+     */
+    private function debug($function, $text = '')
+    {
+        $space = 10 - strlen($function);
+        $space = ($space < 0) ? 0 : $space;
+
+        if ($this->debug) {
+            echo '[' . date('h:i:s A') . "]: $function" . str_repeat(' ', $space);
+            echo(empty($text) ? '' : ': ' . $text) . PHP_EOL;
+        }
+    }
+
+    /**
+     * Curl request
+     *
+     * @param string $url     url request
+     * @param array  $options options yang akan digunakan
+     *                        array  header    untuk setting headernya
+     *                        string useragent untuk set useragent
+     *                        string method    'post', 'put', 'delete'
+     */
+    public function request($url, $options = [])
+    {
+        $userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.87 Safari/537.36';
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_MAXREDIRS, 3);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookie.txt');
+        curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookie.txt');
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($ch, CURLOPT_USERAGENT, $userAgent);
+
+        if ($this->proxy) {
+            curl_setopt($ch, CURLOPT_PROXY, $this->proxy);
+        }
+
+        if (!empty($options)) {
+            if (isset($options['header'])) {
+                curl_setopt($ch, CURLOPT_HTTPHEADER, $options['header']);
+            } elseif (isset($options['useragent'])) {
+                curl_setopt($ch, CURLOPT_USERAGENT, $options['useragent']);
+            } elseif (isset($options['method'])) {
+                if (strtolower($options['method']) == 'post') {
+                    curl_setopt($ch, CURLOPT_POST, true);
+                    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($options['postdata']));
+                } elseif (strtolower($options['method']) == 'delete') {
+                    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+                } elseif (strtolower($options['method']) == 'put') {
+                    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+                    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($options['postdata']));
+                }
+            }
+        }
+
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+        $html           = curl_exec($ch);
+        $this->httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        return $html;
+    }
+}
